2045f9b2cfaaaba6360a6e814061c3a2657cc04b
Dhruv Patel <dhruv225@outlook.com>
2023-05-30 17:43:35 -0400

    Add log-in post and get routes

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,10 @@ router.get("/sign-up", userController.sign_up_get);
 // Post sign up form
 router.post("/sign-up", userController.sign_up_post);
 
+// Get log in form
+router.get("/log-in", userController.log_in_get);
+
+// Post log in form
+router.post("/log-in", userController.log_in_post);
+
 module.exports = router;
